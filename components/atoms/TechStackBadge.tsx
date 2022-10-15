18c6266cab28d140954b1b4a2788bd2f38ddd76f
mori8 <kaithape@gmail.com>
2022-10-15 15:24:49 +0900

    [Implement] TechStackBadge

---
+++ components/atoms/TechStackBadge.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Image from 'next/image';
+import classNames from 'classnames';
+
+export interface ITechStackBadgeProps {
+  tech: string;
+}
+
+export default function TechStackBadge (props: ITechStackBadgeProps) {
+  const { tech } = props;
+  return (
+    <div className={classNames('w-fit h-fit flex flex-row text-[10px] items-center rounded-lg rounded-bl-none px-[10px] py-[2px]', {
+      'bg-react': tech === 'React',
+      'bg-fastAPI text-white': tech === 'fastAPI',
+      'bg-aws text-white': tech === 'AWS',
+      'bg-typescript text-white': tech === 'Typescript',
+      'bg-tailwind text-white': tech === 'tailwind',
+    })}>
+      <Image src={`/images/tech/${tech}.svg`} alt={tech} width={12} height={12}/>
+      <span className='ml-1'>{tech}</span>
+    </div>
+  );
+}
