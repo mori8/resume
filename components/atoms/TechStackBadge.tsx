9e8e5ce9f718d77d81a7f910380c9146f355042d
mori8 <kaithape@gmail.com>
2022-11-12 23:51:16 +0900

    Refactor: techbadge에 사용하는 tailwind theme colors 삭제

--- components/atoms/TechStackBadge.tsx
+++ components/atoms/TechStackBadge.tsx
@@ -11,25 +11,26 @@ export default function TechStackBadge(props: ITechStackBadgeProps) {
   return (
     <div
       className={classNames(
-        "w-fit h-fit flex flex-row text-[10px] items-center rounded-lg rounded-bl-none px-[8px] py-[2px]",
+        "w-fit h-fit flex flex-row text-[10px] items-center rounded-lg rounded-bl-none leading-2 px-[8px] py-[2px]",
         {
-          "bg-react": tech === "React",
-          "bg-fastAPI text-white": tech === "fastAPI",
-          "bg-aws text-white": tech === "AWS",
-          "bg-typescript text-white": tech === "TypeScript",
-          "bg-tailwind text-white": tech === "tailwind",
-          "bg-mysql text-white": tech === "MySQL",
-          "bg-heroku text-white": tech === "Heroku",
-          "bg-sass text-white": tech === "Sass",
+          "bg-[#61DAFB]": tech.split(' ')[0] === "React",
+          "bg-[#009688] text-white": tech === "fastAPI",
+          "bg-[#232F3E] text-white": tech === "AWS",
+          "bg-[#3178C6] text-white": tech === "TypeScript",
+          "bg-[#38B2AC] text-white": tech === "tailwind",
+          "bg-[#4479A1] text-white": tech === "MySQL",
+          "bg-[#430098] text-white": tech === "Heroku",
+          "bg-[#CC6699] text-white": tech === "Sass",
           "bg-black text-white": tech === "Nextjs",
+          "bg-[#000020] text-white": tech === "Expo",
         }
       )}
     >
       <Image
-        src={`/images/tech/${tech}.svg`}
+        src={`/images/tech/${tech.split(' ')[0]}.svg`}
         alt={tech}
-        width={12}
-        height={12}
+        width={10}
+        height={10}
       />
       <span className="ml-1">{tech}</span>
     </div>
