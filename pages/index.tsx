1c7869175fbad8502e6e6b6ff1e16247c0a19732
mori8 <kaithape@gmail.com>
2023-05-04 12:44:19 +0900

    Set: open-graph data

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,10 @@ const Home: NextPage = () => {
     <div className="m-0 p-0">
       <Head>
         <title>남수연 | Suyeon Nam</title>
+        <meta property="og:title" content="남수연 | Suyeon Nam" />
+        <meta property="og:description" content="Software Engineer" />
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:image" content="/og-thumbnail.jpg"/>
       </Head>
 
       <ResumeTemplate />
