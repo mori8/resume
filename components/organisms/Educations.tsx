1f7d4c184ea9a94ac487a2977f7349b53ced29f1
mori8 <kaithape@gmail.com>
2024-02-27 21:12:13 +0900

    update: education status

--- components/organisms/Educations.tsx
+++ components/organisms/Educations.tsx
@@ -9,29 +9,37 @@ export default function Educations() {
       <HighlightedText backgroundSize="none" className="text-2xl mb-10">
         Educations
       </HighlightedText>
-      <div className="flex-1 flex flex-col gap-2">
-        <div>
-          <div className="flex flex-row gap-4">
-            <h3 className="text-xl font-bold">{educations.univ}</h3>
-            <h5 className="text-xl">{educations.major}</h5>
+      <div className="flex flex-col gap-8">
+        {educations.map((education, index) => (
+          <div className="flex-1 flex flex-col">
+            <div>
+              <div className="flex flex-row gap-4">
+                <h3 className="text-xl font-bold">{education.univ}</h3>
+                <h5 className="text-xl">{education.major}</h5>
+              </div>
+              <div className="flex flex-row gap-3">
+                {education.totalGrade && (
+                  <span>
+                    GPA <b>{education.totalGrade}</b>
+                  </span>
+                )}
+                {education.majorGrade && (
+                  <span>
+                    MAJOR GPA <b>{education.majorGrade}</b>
+                  </span>
+                )}
+              </div>
+              <div>
+                <span className="period">{education.period}</span>
+              </div>
+            </div>
+            <ul className="text-gray-600 ul-disc">
+              {education.details.map((detail, index) => (
+                <CustomLi key={`education_${index}`}>{detail}</CustomLi>
+              ))}
+            </ul>
           </div>
-          <div className="flex flex-row gap-3">
-            <span>
-              GPA <b>{educations.totalGrade}</b>
-            </span>
-            <span>
-              MAJOR GPA <b>{educations.majorGrade}</b>
-            </span>
-          </div>
-          <div>
-            <span className="period">{educations.period}</span>
-          </div>
-        </div>
-        <ul className="text-gray-600 ul-disc">
-          {educations.details.map((detail, index) => (
-            <CustomLi key={`education_${index}`}>{detail}</CustomLi>
-          ))}
-        </ul>
+        ))}
       </div>
     </section>
   );
